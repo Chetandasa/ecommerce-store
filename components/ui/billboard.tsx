1908a8668c433f77ba91e5f8d158b79f43b954b8
Your Name <chetandasa2814@gmail.com>
2023-07-05 21:35:27 +0530

    billboard change

--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -1,16 +1,22 @@
 import { Billboard } from "@/types";
+import React from "react";
 
 interface BillboardProps {
   data: Billboard;
 }
 
-const Billboard: React.FC<BillboardProps> = ({
-  data
-}) => {
-  console
-  return ( 
+const Billboard: React.FC<BillboardProps> = ({ data }) => {
+  return (
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
-      <div style={{ backgroundImage: `url(${data?.imageUrl})` }} className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
+      <div
+        style={{
+          backgroundImage: `url(${data?.imageUrl})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden"
+      >
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
           <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
             {data.label}
@@ -18,7 +24,32 @@ const Billboard: React.FC<BillboardProps> = ({
         </div>
       </div>
     </div>
-   );
+  );
 };
 
 export default Billboard;
+
+// import { Billboard } from "@/types";
+
+// interface BillboardProps {
+//   data: Billboard;
+// }
+
+// const Billboard: React.FC<BillboardProps> = ({
+//   data
+// }) => {
+//   console
+//   return ( 
+//     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
+//       <div style={{ backgroundImage: `url(${data?.imageUrl})` }} className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
+//         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
+//           <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
+//             {data.label}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//    );
+// };
+
+// export default Billboard;
